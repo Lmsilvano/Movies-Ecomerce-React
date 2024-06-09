f75685af61dd9ce03f368f000255032be8b4f02f
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2024-06-08 21:45:48 -0300

    Update index.js

--- src/Components/HomeContainer/index.js
+++ src/Components/HomeContainer/index.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const HomeContainer = () => {
     return (
-        <div>HomeContainer</div>
+        <div>HomeContaine
+        
+     <iframe
+        src="https://vk.com/video_ext.php?oid=259866354&id=456239135&hash=cd1f68f175c367d6"
+        width="426"
+        height="240"
+        allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        src="https://vk.com/video_ext.php?oid=259866354&id=456239383&hd=2&hash=ce5003535f742af2"
+        width="853"
+        height="480"
+        allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
     )
 }
 export default HomeContainer
